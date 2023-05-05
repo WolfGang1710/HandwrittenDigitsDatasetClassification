6eae480cc67ee8788ebfd3938fc78957e0b2b5c0
Thibaut <hertzogthibaut@gmail.com>
2023-05-05 10:49:53 +0200

    Premier code

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ import matplotlib.pyplot as plt
 import scipy.special
 import scipy.special as sp
 import scipy.optimize as opt
+import sklearn.model_selection
+import sklearn.datasets
+
+#Définition des constantes
+X = sklearn.datasets.load_digits().data
+Y = sklearn.datasets.load_digits().target
+N = len(X)
+d = len(X[0])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2)
+
+x_train_ones = np.column_stack((np.ones(len(x_train)), x_train))
+x_test_ones = np.column_stack((np.ones(len(x_test)), x_test))
+
+y_train = np.asarray(y_train, dtype=float)
+y_test = np.asarray(y_test, dtype=float)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def g(theta):
+    return sigmoid(np.dot(x_train_ones, theta))
