bce728d51d97fff9c6ae0c427c7aa5c1527fc778
Willim ROGUET <20626269+WolfGang1710@users.noreply.github.com>
2023-05-05 10:22:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+@author : HERTZOG Thibaut & ROGUET William
+"""
+
+#Importation des librairies nécessaires
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special
+import scipy.special as sp
+import scipy.optimize as opt
